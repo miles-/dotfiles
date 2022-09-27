3c6626d1a2cbc86102d557adc9c8499491fcdb52
Miles Fricker <miles.fricker@gmail.com>
2022-09-27 10:15:53 -0400

    Include hidden files in nvim shortcut

--- nvim/config.lua
+++ nvim/config.lua
@@ -67,7 +67,7 @@ require('telescope').setup {
   },
 }
 -- add leader shortcuts
-vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files({hidden = true})<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
