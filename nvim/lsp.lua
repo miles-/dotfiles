6be603fabbf0b279c4fb7ca3051f145eff31b745
Miles Fricker <miles.fricker@gmail.com>
2022-10-10 10:29:04 -0400

    Add bash lsp

--- nvim/lsp.lua
+++ nvim/lsp.lua
@@ -35,7 +35,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'terraform_lsp', 'pyright', 'tsserver', 'rls', 'dockerls', 'jsonls', 'gopls', 'sqls', 'cssls', 'ltex' }
+local servers = { 'terraform_lsp', 'pyright', 'tsserver', 'rls', 'dockerls', 'jsonls', 'gopls', 'sqls', 'cssls', 'ltex', 'bashls' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
