d4800eb50cdc4a1318cb66173b79d284f3833453
Miles Fricker <miles.fricker@gmail.com>
2023-09-18 09:00:53 -0400

    updates

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -15,6 +15,7 @@ lsp.ensure_installed({
   'sumneko_lua',
   'tailwindcss',
   'terraformls',
+  'tflint',
   'tsserver',
 })
 
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,6 +2,20 @@
 require('telescope').setup {
   defaults = {
     layout_strategy = 'flex',
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '-u'
+    },
+    file_ignore_patterns = {
+      'node_modules',
+      '.git'
+    },
   },
 }
 -- add leader shortcuts
