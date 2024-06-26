b015df5529f3b710542514394814841515d3deb4
Miles Fricker <miles.fricker@gmail.com>
2024-06-26 11:11:57 -0400

    aerospace updates

--- sketchybar/plugins/spotify_indicator.sh
+++ sketchybar/plugins/spotify_indicator.sh
@@ -15,9 +15,9 @@ if [ "$(osascript -e 'if application "Spotify" is running then tell application 
 fi
 if [ $RUNNING -eq 0 ] && [ $PLAYING -eq 0 ]; then
   if [ "$ARTIST" == "" ]; then
-    sketchybar --set $NAME label=" $TRACK - $ALBUM" --set '/spot.*/' drawing=on
+    sketchybar --set $NAME label="$TRACK - $ALBUM" --set '/spot.*/' drawing=on
   else
-    sketchybar --set $NAME label=" $TRACK - $ARTIST" --set '/spot.*/' drawing=on
+    sketchybar --set $NAME label="$TRACK - $ARTIST" --set '/spot.*/' drawing=on
   fi
 else
   sketchybar --set '/spot.*/' drawing=off
